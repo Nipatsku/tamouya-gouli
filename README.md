a38b9e10b9dbce2e1bacc267c558ee7f8e4ac938
Nipatsku <niilo.keinanen@hotmail.com>
2020-04-04 20:28:38 +0300

    Speech to text with all language codes of google

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 
 https://www.youtube.com/watch?v=R9RnxWmPkxo
 
-128kbps
+
+
+# Memo
+
 FLAC
 0:00 -> 1:17 = cutted
+MONO audio.
+https://cloud.google.com/speech-to-text/
+https://cloud.google.com/speech-to-text/docs/languages
+https://en.wikipedia.org/wiki/Languages_of_Guinea
+https://console.cloud.google.com/home/dashboard?project=tamouya-gouli
--- test.js
+++ test.js
@@ -1,14 +1,19 @@
 async function main() {
-    // Imports the Google Cloud client library
+    require('dotenv').config()
     const speech = require('@google-cloud/speech');
     const fs = require('fs');
+    const languageCodes = require('./languageCodes.json')
   
     // Creates a client
-    const client = new speech.SpeechClient();
+    const client = new speech.SpeechClient({
+        projectId: process.env.PROJECT_ID,
+        keyFilename: process.env.KEY_FILE
+    });
   
     // The name of the audio file to transcribe
-    const fileName = 'test.flac';
-    const inFile = `./resources/${fileName}`
+    const fileName = '012';
+    const fileFormat = 'flac'
+    const inFile = `./resources/${fileName}.${fileFormat}`
   
     // Reads a local audio file and converts it to base64
     const file = fs.readFileSync( inFile );
@@ -18,24 +23,30 @@ async function main() {
     const audio = {
       content: audioBytes,
     };
-    const config = {
-      encoding: 'FLAC',
-      languageCode: 'fi-FI',
-    };
-    const request = {
-      audio: audio,
-      config: config,
-    };
-  
-    // Detects speech in the audio file
-    const [response] = await client.recognize(request);
-    const transcription = response.results
-      .map(result => result.alternatives[0].transcript)
-      .join('\n');
-    console.log(`Transcription: ${transcription}`);
 
-    const outFile = `./output/${fileName}`
-    console.log(`Writing to ${outFile} ...`)
-    fs.writeFileSync( outFile, JSON.stringify( transcription ) )
+    for ( let i = 0; i < languageCodes.length ; i ++ ) {
+        const languageCode = languageCodes[i]
+        console.log( `${i}/${languageCodes.length-1} | ${languageCode}` )
+        const config = {
+            encoding: 'FLAC',
+            languageCode
+        };
+        const request = {
+        audio: audio,
+        config: config,
+        };
+    
+        // Detects speech in the audio file
+        const [response] = await client.recognize(request);
+        const transcription = response.results
+            .map(result => result.alternatives[0].transcript)
+            .join('\n');
+        console.log(`Transcription: ${transcription}`);
+    
+        const outFile = `./output/${fileName}_${languageCode}.json`
+        console.log(`Writing to ${outFile} ...`)
+        fs.writeFileSync( outFile, JSON.stringify( transcription ) )
+    }
+
 }
 main().catch(console.error);
