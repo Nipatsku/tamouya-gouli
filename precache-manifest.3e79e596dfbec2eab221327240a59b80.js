ee5a9bcd7d81e7914e6a18cbfcbf99bb8ac57a37
Nipatsku <niilo.keinanen@hotmail.com>
2020-04-07 21:12:39 +0300

    Updates

---
+++ precache-manifest.3e79e596dfbec2eab221327240a59b80.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "26079a4d82ed58a4aadd",
+    "url": "/tamouya-gouli/static/js/runtime~main.26079a4d.js"
+  },
+  {
+    "revision": "6cc87d368a2c04227c79",
+    "url": "/tamouya-gouli/static/js/main.6cc87d36.chunk.js"
+  },
+  {
+    "revision": "86e2be0ac3b5b78232af",
+    "url": "/tamouya-gouli/static/js/1.86e2be0a.chunk.js"
+  },
+  {
+    "revision": "6cc87d368a2c04227c79",
+    "url": "/tamouya-gouli/static/css/main.64c15de5.chunk.css"
+  },
+  {
+    "revision": "ddc29c801cb7922feb23572370830d3a",
+    "url": "/tamouya-gouli/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/tamouya-gouli/precache-manifest.71e46288d0467230a63834f963fb5a1d.js"
+  "/tamouya-gouli/precache-manifest.3e79e596dfbec2eab221327240a59b80.js"
 );
 
 workbox.clientsClaim();
