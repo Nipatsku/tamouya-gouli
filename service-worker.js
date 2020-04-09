7d813e2939551698a58401112cc81e2981998528
Nipatsku <niilo.keinanen@hotmail.com>
2020-04-09 21:33:11 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/tamouya-gouli/precache-manifest.0b95999d51436df947770a23911b6679.js"
+  "/tamouya-gouli/precache-manifest.d6eb8e478dbb15814c03d826b28f0f5c.js"
 );
 
 workbox.clientsClaim();
