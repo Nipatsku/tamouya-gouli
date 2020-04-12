25420263e36d91682ec565d950f50a39b3fdf8fa
Nipatsku <niilo.keinanen@hotmail.com>
2020-04-12 17:19:14 +0300

    Updates

---
+++ precache-manifest.3ff0b21eee05adca8c8c21ca531c6559.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "26079a4d82ed58a4aadd",
+    "url": "/tamouya-gouli/static/js/runtime~main.26079a4d.js"
+  },
+  {
+    "revision": "50f0c40c599563972d48",
+    "url": "/tamouya-gouli/static/js/main.50f0c40c.chunk.js"
+  },
+  {
+    "revision": "87220b0efa6387d80674",
+    "url": "/tamouya-gouli/static/js/1.87220b0e.chunk.js"
+  },
+  {
+    "revision": "50f0c40c599563972d48",
+    "url": "/tamouya-gouli/static/css/main.61531971.chunk.css"
+  },
+  {
+    "revision": "27b4f2fb58bd73014242afb84ce86f07",
+    "url": "/tamouya-gouli/index.html"
+  }
+];
